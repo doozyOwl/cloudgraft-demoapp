2c11c468f595f8ff9a8eac5338c0e93d5d54db0c
sathya <bksathyamoorthy@gmail.com>
2025-12-04 10:22:48 +0000

    trigger test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Cloudgraft Demo App! Version 1.0.0\n');
+  res.end('Hello from Cloudgraft Demo App! Version 1.0.0 - v2\n');
 });
 
 const port = process.env.PORT || 3000;
